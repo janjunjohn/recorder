ce522ebb9326d4180ddfdee77c5e0ddacf0a755b
JUN KAKISHIMA <junkakishima@JUNs-MacBook-Pro.local>
2023-12-04 07:57:22 +0900

    refactor: gather import routers

--- src/routers/routers.py
+++ src/routers/routers.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 
-from routers import user_routers
-from routers import task_routers
-from routers import record_routers
+from routers import user_routers, task_routers, record_routers
 
 
 router = APIRouter()
