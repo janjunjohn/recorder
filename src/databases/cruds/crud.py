7426acadafba29b4fb1267e6b342b7e09db8a042
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-12-11 01:37:06 +0900

    feat: Refactor CRUD functions and add exists_user_by_id

--- src/databases/cruds/crud.py
+++ src/databases/cruds/crud.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import Session
 from databases.models.user import User as UserTable
 from schemas.user_schema import User
-from services.common.hash import HashService
 
 
 def get_user(db: Session, user_id: str) -> User:
@@ -14,6 +13,11 @@ def get_user_by_email(db: Session, email: str) -> User:
     if db_user is None:
         return None
     return User(id=db_user.id, email=db_user.email, username=db_user.username, hashed_password=db_user.hashed_password, is_active=db_user.is_active, created_at=db_user.created_at, updated_at=db_user.updated_at)
+
+
+def exists_user_by_id(db: Session, user_id: str) -> bool:
+    query = db.query(UserTable).filter(UserTable.id == user_id)
+    return db.query(query.exists()).scalar()
 
 
 def exists_user_by_email(db: Session, email: str) -> bool:
