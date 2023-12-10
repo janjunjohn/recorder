cae4b98c0ac432e6ebf5c761c126600ba483d460
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-12-11 01:32:48 +0900

    Add user deletion functionality and hash service import

--- src/databases/cruds/crud.py
+++ src/databases/cruds/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from databases.models.user import User as UserTable
 from schemas.user_schema import User
+from services.common.hash import HashService
 
 
 def get_user(db: Session, user_id: str) -> User:
@@ -8,7 +9,23 @@ def get_user(db: Session, user_id: str) -> User:
 
 
 def get_user_by_email(db: Session, email: str) -> User:
-    return db.query(UserTable).filter(UserTable.email == email).first()
+    db_user: UserTable = db.query(UserTable).filter(
+        UserTable.email == email).first()
+    if db_user is None:
+        return None
+    return User(id=db_user.id, email=db_user.email, username=db_user.username, hashed_password=db_user.hashed_password, is_active=db_user.is_active, created_at=db_user.created_at, updated_at=db_user.updated_at)
+
+
+def exists_user_by_email(db: Session, email: str) -> bool:
+    query = db.query(UserTable).filter(UserTable.email == email)
+    return db.query(query.exists()).scalar()
+
+
+def delete_user(db: Session, user_id: str) -> None:
+    db_user = db.query(UserTable).filter(UserTable.id == user_id).first()
+    db_user.is_active = False
+    db.commit()
+    db.refresh(db_user)
 
 
 def create_user(db: Session, user: User) -> User:
