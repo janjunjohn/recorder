a82316623ee6c6b79f2f243522580f4d70a116e7
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-12-11 01:37:20 +0900

    feat: Fix error handling in user_routers.py

--- src/routers/user_routers.py
+++ src/routers/user_routers.py
@@ -4,8 +4,8 @@ from typing import List
 
 from databases.settings.database import get_db
 from schemas.user_schema import UserCreate, User
-from services.common.errors import UserAlreadyExistsError
-from services.user_service import create_user
+from services.common.errors import UserAlreadyExistsError, UserNotFoundError
+from services import user_service
 
 
 router = APIRouter(prefix='/users', tags=['users'])
@@ -14,9 +14,8 @@ router = APIRouter(prefix='/users', tags=['users'])
 @router.post("/")
 def signup(user: UserCreate, db: Session = Depends(get_db)) -> User:
     try:
-        return create_user(db, user)
+        return user_service.create_user(db, user)
     except UserAlreadyExistsError as e:
-        print(e)
         raise HTTPException(status_code=400, detail=e.args[0])
     except Exception as e:
         raise HTTPException(status_code=500, detail=e)
@@ -24,9 +23,9 @@ def signup(user: UserCreate, db: Session = Depends(get_db)) -> User:
 
 @router.delete("/{user_id}")
 def delete_user(user_id: str, db: Session = Depends(get_db)) -> None:
-    db_user: User = crud.get_user(db, user_id=user_id)
-
-    if not db_user:
-        raise HTTPException(status_code=404, detail="ユーザーが見つかりません")
-
-    return crud.delete_user(db=db, user_id=user_id)
+    try:
+        return user_service.delete_user(db, user_id)
+    except UserNotFoundError as e:
+        raise HTTPException(status_code=404, detail=e.args[0])
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
