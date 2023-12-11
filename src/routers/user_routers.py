5b31781d8b03b0669f8d4f481cec455beb530584
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-12-12 00:50:11 +0900

    fix: Fix error handling in user routers

--- src/routers/user_routers.py
+++ src/routers/user_routers.py
@@ -45,7 +45,9 @@ def get_user(user_id: str, db: Session = Depends(get_db)) -> User:
 def update_password(user_id: str, user_password_update: UserPasswordUpdate, db: Session = Depends(get_db)) -> None:
     try:
         return user_service.update_password(db, user_id, user_password_update)
-    except (UserNotFoundError, ValueError) as e:
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=e.args[0])
+    except UserNotFoundError as e:
         raise HTTPException(status_code=404, detail=e.args[0])
     except Exception as e:
         raise HTTPException(status_code=500, detail=e)
@@ -55,7 +57,9 @@ def update_password(user_id: str, user_password_update: UserPasswordUpdate, db: 
 def update_user(user_id: str, user_info: UserBase, db: Session = Depends(get_db)) -> None:
     try:
         return user_service.update_user(db, user_id, user_info)
-    except (UserNotFoundError, ValueError) as e:
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=e.args[0])
+    except UserNotFoundError as e:
         raise HTTPException(status_code=404, detail=e.args[0])
     except Exception as e:
         raise HTTPException(status_code=500, detail=e)
