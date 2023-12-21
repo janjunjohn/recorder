241f4e8c143bfddef75ea084572c81a7ba8b3d06
JUN KAKISHIMA <junkakishima@JUNs-MacBook-Pro.local>
2023-12-21 09:16:27 +0900

    feat: add get task list api

--- src/routers/task_routers.py
+++ src/routers/task_routers.py
@@ -4,7 +4,7 @@ from typing import List
 
 from databases.settings.database import get_db
 from schemas.task_schema import TaskCreate, Task
-from services.common.errors import TaskAlreadyExistsError, TaskNotFoundError
+from services.common.errors import TaskAlreadyExistsError, TaskNotFoundError, InvalidUUIDError
 from services import task_service
 
 
@@ -19,3 +19,11 @@ def create_task(task: TaskCreate, db: Session = Depends(get_db)) -> Task:
         raise HTTPException(status_code=400, detail=e.args[0])
     except Exception as e:
         raise HTTPException(status_code=500, detail=e)
+
+
+@router.get("/{user_id}")
+def get_task_list(user_id: str, db: Session = Depends(get_db)) -> List[Task]:
+    try:
+        return task_service.get_task_list_by_user_id(db, user_id) 
+    except InvalidUUIDError as e:
+        raise HTTPException(status_code=400, detail=e.args[0])
