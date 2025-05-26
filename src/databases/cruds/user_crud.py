952d8849670da966ac77797453a30828a360e63b
JUN KAKISHIMA <junkakishima@JUNs-MacBook-Pro.local>
2025-05-26 22:11:10 +0900

    refactor user and task api

--- src/databases/cruds/user_crud.py
+++ src/databases/cruds/user_crud.py
@@ -5,8 +5,18 @@ from databases.models.user import User as UserTable
 from models.user.user_id import UserId
 from models.user.user import User
 from models.user.user_password import UserPassword
-from schemas.user_schema import UserCreateRequest, UserPasswordUpdateRequest, UserUpdateRequest
 from services.common.errors import UserNotFoundError
+
+
+def create_user(db: Session, user: User, new_password: str) -> User:
+    db_user: UserTable = UserTable(
+        id=user.id.id, email=user.email, username=user.username, hashed_password=new_password, is_active=user.is_active,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return User(id=UserId(id=db_user.id), email=db_user.email, username=db_user.username,
+                is_active=db_user.is_active, created_at=db_user.created_at, updated_at=db_user.updated_at)
 
 
 def get_user_by_id(db: Session, user_id: UserId) -> User:
@@ -42,27 +52,16 @@ def exists_active_user_by_id(db: Session, user_id: UserId) -> bool:
     return db.query(query.exists()).scalar()
 
 
-def exists_user_by_email(db: Session, email: str) -> bool:
+def exists_active_user_by_email(db: Session, email: str) -> bool:
     query = db.query(UserTable).filter(UserTable.email == email)
     return db.query(query.exists()).scalar()
 
 
-def delete_user(db: Session, user_id: UserId) -> None:
+def update_password(db: Session, user_id: UserId, new_password: str) -> None:
     db_user: UserTable = db.query(UserTable).filter(UserTable.id == user_id.id).one()
-    db_user.is_active = False
+    db_user.hashed_password = new_password
     db.commit()
     db.refresh(db_user)
-
-
-def create_user(db: Session, user: User, new_password: str) -> User:
-    db_user: UserTable = UserTable(
-        id=user.id.id, email=user.email, username=user.username, hashed_password=new_password, is_active=user.is_active,
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return User(id=UserId(id=db_user.id), email=db_user.email, username=db_user.username,
-                is_active=db_user.is_active, created_at=db_user.created_at, updated_at=db_user.updated_at)
 
 
 def update_user(db: Session, user: User) -> User:
@@ -74,8 +73,8 @@ def update_user(db: Session, user: User) -> User:
     return User(id=UserId(id=db_user.id), email=db_user.email, username=db_user.username, is_active=db_user.is_active)
 
 
-def update_password(db: Session, user_id: UserId, new_password: str) -> None:
+def delete_user(db: Session, user_id: UserId) -> None:
     db_user: UserTable = db.query(UserTable).filter(UserTable.id == user_id.id).one()
-    db_user.hashed_password = new_password
+    db_user.is_active = False
     db.commit()
     db.refresh(db_user)
